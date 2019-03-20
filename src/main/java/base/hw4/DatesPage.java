3ae6c14051fc697518f581d3688bc378cc9c95f5
Viktoriia_Fedorova <fjodorova252@rambler.ru>
2019-03-20 15:01:43 +0300

    hw4_10 (update Selenide version)

--- src/main/java/base/hw4/DatesPage.java
+++ src/main/java/base/hw4/DatesPage.java
@@ -1,8 +1,12 @@
 package base.hw4;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class DatesPage {
 
@@ -13,7 +17,12 @@ public class DatesPage {
     public static ElementsCollection sliderHandles;
 
     public void setSliderHandlesToItsExtremes(){
-        sliderHandles.get(0).setValue("0");
+
+//        $("#similarityv").shouldHave(text(“0.8”));
+//        executeJavaScript(“document.getElementById(‘Similarity’).style.display = ‘inline’;”);
+//        $("#Similarity").val(“0.7”);
+
+        sliderHandles.get(0).shouldHave(Condition.text());
         sliderHandles.get(1).setValue("100");
     }
 }
