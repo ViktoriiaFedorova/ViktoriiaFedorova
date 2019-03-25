1a924fd42b262b6955d0eb31a942700eaae99fa2
Viktoriia_Fedorova <fjodorova252@rambler.ru>
2019-03-26 01:12:06 +0300

    hw4_13

--- src/main/java/base/hw4/DatesPage.java
+++ src/main/java/base/hw4/DatesPage.java
@@ -3,94 +3,47 @@ package base.hw4;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.WebDriverRunner;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
-import java.awt.*;
+import static com.codeborne.selenide.Condition.text;
 
 public class DatesPage {
 
-    @FindBy(css = "[class^='uui-slider blue']")
-    public static SelenideElement sliderLine;
+    @FindBy(css = "[class^='uui-slider']")
+    private SelenideElement slider;
 
-    @FindBy(css = "[class^='ui-slider-handle']:first-child")
-    public static SelenideElement slider;
+    @FindBy(css = "a.ui-slider-handle:nth-child(1)")
+    public static SelenideElement leftSliderHandle;
 
-    public void setSliderHandlesToItsExtremes() {
+    @FindBy(css = "a.ui-slider-handle:nth-child(3)")
+    public static SelenideElement rightSliderHandle;
 
-//        $("#similarityv").shouldHave(text(“0.8”));
-//        executeJavaScript(“document.getElementById(‘Similarity’).style.display = ‘inline’;”);
-//        $("#Similarity").val(“0.7”);
+    @FindBy(css = "[class='panel-body-list logs'] li")
+    public static ElementsCollection log;
 
-        //slider.get(0).shouldHave(Condition.text("0"));
-        //slider.get(1).setValue("100");
+    public void setSliderHandleToPosition(SelenideElement sliderHandle, int position) {
 
-        //632(570) and 880 248
-//        int i = (int) (2 * 3.1);
-//        int delta = i - i/30;
-//        Selenide.actions().clickAndHold(slider.get(0)).moveByOffset(delta,0).release().build().perform();
-//        System.out.println();
+        int offset = slider.getSize().width * (position - Integer.valueOf(sliderHandle.lastChild().getText())) / 100 - 1;
+        Selenide.actions().clickAndHold(sliderHandle).moveByOffset(offset, 0).release().build().perform();
 
-        int sliderLength = sliderLine.getSize().width;
-        int sliderStart = sliderLine.getLocation().x;
+    }
 
-        int sliderStep = sliderLength / 100;
+    public void checkLogForSliderHandlePosition(SelenideElement sliderHandle, int position) {
 
-        int handleSize = slider.getSize().width;
-        int handleLocation = slider.getLocation().x + handleSize / 2;
-//        Selenide.actions().clickAndHold(slider.get(0)).moveByOffset(- (handleLocation - sliderStart), 0).release().build().perform();
-//
-//        int newHandleLocation = slider.get(0).getLocation().x;
-//        if (newHandleLocation != (sliderStart - 10) && !slider.get(0).getAttribute("style").contains(" 0%")){
-//            Selenide.actions().clickAndHold(slider.get(0)).moveByOffset(- handleSize/2, 0).release().build().perform();
-//        }
-//
-//        int targetPosition = 56;
-//        Selenide.actions().clickAndHold(slider.get(0)).moveByOffset(sliderStep * targetPosition, 0).release().build().perform();
-//
-//        int nextHandleLocation = slider.get(0).getLocation().x;
-//        if (nextHandleLocation != (sliderStart - 10 + sliderStep * targetPosition) && !slider.get(0).getAttribute("style").contains(" 56%")){
-//            Selenide.actions().clickAndHold(slider.get(0)).moveByOffset(- (nextHandleLocation - sliderStart - 10 + sliderStep * targetPosition), 0).release().build().perform();
-//        }
-//        int lastHandleLocation = slider.get(0).getLocation().x;
-//
-//        System.out.println("end1");
+        String nameForLog = "";
+        if (sliderHandle == leftSliderHandle)
+            nameForLog = "From";
+        else if (sliderHandle == rightSliderHandle)
+            nameForLog = "To";
 
-//        int location1 = slider.get(0).getLocation().x;
-//        Selenide.actions().clickAndHold(slider.get(0)).moveByOffset(50, 0).release().build().perform();
-//        int location2 = slider.get(0).getLocation().x;
-//        if (location2 != location1 + 50)
-//            Selenide.actions().clickAndHold(slider.get(0)).moveByOffset(location1 + 50 - location2, 0).release().build().perform();
-//        int location3 = slider.get(0).getLocation().x;
-
-        Actions actions = Selenide.actions();
-        Action action = null;
-        while (true) {
-            System.out.println(slider.getLocation());
-            System.out.println(slider.getText());
-
-            Actions actions1 = Selenide.actions().moveToElement(slider);
-            int x = 17;
-            if (!slider.getText().equals(String.valueOf(x))) {
-//                int y = -1;
-                System.out.println(MouseInfo.getPointerInfo().getLocation());
-
-                actions1.clickAndHold().moveByOffset(-1,0).build().perform();
-
-                System.out.println(MouseInfo.getPointerInfo().getLocation());
-                System.out.println(slider.getLocation());
-            } else {
-                actions1.release(slider).build().perform();
-                System.out.println(slider.getLocation());
-                System.out.println("Конченный getLocation:" + slider);
+        for (SelenideElement logRecord : log) {
+            if (logRecord.getText().contains(nameForLog)) {
+                logRecord.should(text(Integer.valueOf(position).toString()));
                 break;
+                //System.out.println("log checked");
             }
         }
-        System.out.println("dsdfsdf");
 
-
-//        System.out.println("end2");
+        //System.out.println("log method");
     }
 }
--- src/test/java/hw4/TestForSlidersOnDatesPage.java
+++ src/test/java/hw4/TestForSlidersOnDatesPage.java
@@ -3,15 +3,20 @@ package hw4;
 import base.hw4.DatesPage;
 import base.hw4.IndexPageSelenide;
 import base.hw4.SelenideBase;
+import com.codeborne.selenide.testng.SoftAsserts;
 import enums.IndexPageTexts;
 import enums.Users;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+import static base.hw4.DatesPage.leftSliderHandle;
+import static base.hw4.DatesPage.rightSliderHandle;
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
 
+@Listeners({SoftAsserts.class})
 public class TestForSlidersOnDatesPage extends SelenideBase {
 
     private IndexPageSelenide indexPageSelenide;
@@ -46,15 +51,44 @@ public class TestForSlidersOnDatesPage extends SelenideBase {
         datesPage = indexPageSelenide.openDatesPagefromHeader();
 
         //step 6: set Range sliders: left is on the most left position, right - the most right position
-        datesPage.setSliderHandlesToItsExtremes();
+        datesPage.setSliderHandleToPosition(leftSliderHandle, 0);
+        datesPage.setSliderHandleToPosition(rightSliderHandle, 100);
 
         //step 7: assert there are log rows for 'From' and 'To' sliders with corresponding values (method should be parametrized)
+        datesPage.checkLogForSliderHandlePosition(leftSliderHandle, 0);
+        datesPage.checkLogForSliderHandlePosition(rightSliderHandle, 100);
+
         //step 8: set Range sliders: left is on the most left position, right - the most left position
+        datesPage.setSliderHandleToPosition(leftSliderHandle, 0);
+        datesPage.setSliderHandleToPosition(rightSliderHandle, 0);
+
         //step 9: assert there are log rows for 'From' and 'To' sliders with corresponding values (method should be parametrized)
+        datesPage.checkLogForSliderHandlePosition(leftSliderHandle, 0);
+        datesPage.checkLogForSliderHandlePosition(rightSliderHandle, 0);
+
         //step 10: set Range sliders: left is on the most right position, right - the most right position
+        datesPage.setSliderHandleToPosition(rightSliderHandle, 100);
+        datesPage.setSliderHandleToPosition(leftSliderHandle, 100);
+
+        System.out.println("log1");
+
         //step 11: assert there are log rows for 'From' and 'To' sliders with corresponding values (method should be parametrized)
+        datesPage.checkLogForSliderHandlePosition(rightSliderHandle, 100);
+        datesPage.checkLogForSliderHandlePosition(leftSliderHandle, 100);
+
+        System.out.println("log2");
+
         //step 12: set Range sliders: left is 30, right is 70
+        datesPage.setSliderHandleToPosition(leftSliderHandle, 30);
+        datesPage.setSliderHandleToPosition(rightSliderHandle, 70);
+
+        System.out.println("log3");
+
         //step 13: assert there are log rows for 'From' and 'To' sliders with corresponding values (method should be parametrized)
+        datesPage.checkLogForSliderHandlePosition(leftSliderHandle, 30);
+        datesPage.checkLogForSliderHandlePosition(rightSliderHandle, 70);
+
+        System.out.println("log");
 
     }
 
