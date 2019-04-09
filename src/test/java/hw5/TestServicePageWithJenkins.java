30dcc04cb4e763fa87e60db5118ab5c5ae950df8
Viktoriia_Fedorova <fjodorova252@rambler.ru>
2019-04-09 20:55:37 +0300

    hw5_4 (change selenoid address)

--- src/test/java/hw5/TestServicePageWithJenkins.java
+++ src/test/java/hw5/TestServicePageWithJenkins.java
@@ -42,7 +42,8 @@ public class TestServicePageWithJenkins extends SelenideBase {
             desiredCapabilities.setBrowserName("chrome");
             desiredCapabilities.setVersion("73");
             desiredCapabilities.setCapability("screenResolution", "1920x1080x24");
-            Configuration.remote = "http://192.168.1.72:4444/wd/hub"; //local selenoid
+//            Configuration.remote = "http://192.168.1.72:4444/wd/hub"; //local selenoid
+            Configuration.remote = "http://192.168.99.100:4444/wd/hub"; //local selenoid
             Configuration.browserCapabilities = desiredCapabilities;
         }
 
